46010f1bfb3058d090ab6c8ce02b12de202b32c3
risheng1996 <rishengt@gmail.com>
2020-05-02 20:18:18 -0400

    感觉这道题再做多几次都还会错。。。easy题你逗我？？？

--- byName/src/ArrayFindAllNumbersDisappearedin.java
+++ byName/src/ArrayFindAllNumbersDisappearedin.java
@@ -1,3 +1,10 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
  *
@@ -14,4 +21,20 @@
  * [5,6]
  */
 public class ArrayFindAllNumbersDisappearedin {
+
+    public static void main(String[] args) {
+        System.out.println(new ArrayFindAllNumbersDisappearedin().findDisappearedNumbers(new int[]{4,3,2,7,8,2,3,1}));
+    }
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for(int i = 0; i<nums.length; i++){
+            int moveto = Math.abs(nums[i]);
+            if(nums[moveto-1]<0) continue;
+            nums[moveto-1] *= -1;/***把数组的数跟index联系起来然后变负， 细品，下次估计还是做不出来。。。。。*/
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 1; i <=nums.length; i++){
+            if(nums[i-1]>0) ans.add(i);
+        }
+        return ans;
+    }
 }
