0a3ad62f2358057e1ad6f75dec78667d4bc87511
risheng1996 <rishengt@gmail.com>
2020-06-05 21:07:38 -0400

    传说中的union find哦

---
+++ byName/src/UnionFind.java
@@ -0,0 +1,67 @@
+/**
+ * 1、用 parent 数组记录每个节点的父节点，相当于指向父节点的指针，所以 parent 数组内实际存储着一个森林（若干棵多叉树）。
+ *
+ * 2、用 size 数组记录着每棵树的重量，目的是让 union 后树依然拥有平衡性，而不会退化成链表，影响操作效率。
+ *
+ * 3、在 find 函数中进行路径压缩，保证任意树的高度保持在常数，使得 union 和 connected API 时间复杂度为 O(1)。
+ */
+public class UnionFind {
+    //记录每个连通分量个数
+    private int count;
+
+    //储存若干颗树
+    private int parent[];
+
+    // 记录树的“重量”
+    private int[] size;
+
+    public UnionFind(int n) {
+        this.count = n;
+        parent = new int[n];//刚建立的时候每个节点都是杜立的
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;//每个节点都是一棵树
+            size[i] = 1;
+        }
+    }
+
+    /* 将 p 和 q 连通 */
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ)
+            return;
+
+        // 小树接到大树下面，较平衡
+        if (size[rootP] > size[rootQ]) {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        } else {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        count--;
+    }
+
+    /* 判断 p 和 q 是否互相连通 */
+    public boolean connected(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        // 处于同一棵树上的节点，相互连通
+        return rootP == rootQ;
+    }
+
+    /* 返回节点 x 的根节点 */
+    private int find(int x) {
+        while (parent[x] != x) {
+            // 进行路径压缩
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+
+    public int count() {
+        return count;
+    }
+}
