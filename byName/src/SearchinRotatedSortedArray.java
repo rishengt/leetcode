68877313442e5c55ca666793df4a17514f78098c
risheng1996 <rishengt@gmail.com>
2020-06-12 22:54:59 -0400

    继续二分查找，有序数组旋转后中间切一刀肯定有一半还是有序的

---
+++ byName/src/SearchinRotatedSortedArray.java
@@ -0,0 +1,50 @@
+/**
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ *
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ *
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ *
+ * You may assume no duplicate exists in the array.
+ *
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * Example 1:
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * Example 2:
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ */
+public class SearchinRotatedSortedArray {
+    /**这题的精华是，如果是一条旋转过的有序数组，你从中间切开，肯定有一半是有序的*/
+
+    public static void main(String[] args) {
+        System.out.println(new SearchinRotatedSortedArray().SearchinRotatedSortedArray(new int[]{4,5,6,7,0,1,2},0));
+        System.out.println(new SearchinRotatedSortedArray().SearchinRotatedSortedArray(new int[]{4,5,6,7,0,1,2},3));
+    }
+    public int SearchinRotatedSortedArray(int [] nums, int target){
+        int start = 0, end = nums.length-1;
+        while(start<=end){
+            int mid = (start + end) / 2;
+            if(target == nums[mid]) return mid;
+            //左边有序
+            else if(nums[start] <= nums[mid] ){
+                if(target>=nums[start]&&target<nums[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid+1;
+                }
+            }else{//右边有序
+                if(target > nums[mid] && target<=nums[end]){
+                    start = mid+1;
+                }else{
+                    end = mid -1;
+                }
+            }
+        }
+        return -1;
+    }
+}
