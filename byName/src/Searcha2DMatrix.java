d5e3a01e5379f2b405c4c633054f6510a749b33d
risheng1996 <rishengt@gmail.com>
2020-06-27 17:49:31 -0400

    凡是2d matrix都想想要不要设置两个指针，row 和col。。。。你真他妈菜啊菜🐔

---
+++ byName/src/Searcha2DMatrix.java
@@ -0,0 +1,50 @@
+/**
+ * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+ *
+ * Integers in each row are sorted from left to right.
+ * The first integer of each row is greater than the last integer of the previous row.
+ * Example 1:
+ *
+ * Input:
+ * matrix = [
+ *   [1,   3,  5,  7],
+ *   [10, 11, 16, 20],
+ *   [23, 30, 34, 50]
+ * ]
+ * target = 3
+ * Output: true
+ * Example 2:
+ *
+ * Input:
+ * matrix = [
+ *   [1,   3,  5,  7],
+ *   [10, 11, 16, 20],
+ *   [23, 30, 34, 50]
+ * ]
+ * target = 13
+ * Output: false
+ */
+public class Searcha2DMatrix {
+    public static void main(String[] args) {
+        System.out.println(new Searcha2DMatrix().searchMatrix(new int[][]{},3));
+        System.out.println(new Searcha2DMatrix().searchMatrix(new int[][]{{1,   3,  5,  7},{10, 11, 16, 20},{23, 30, 34, 50}},3));
+        System.out.println(new Searcha2DMatrix().searchMatrix(new int[][]{{1,   3,  5,  7},{10, 11, 16, 20},{23, 30, 34, 50}},13));
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+}
