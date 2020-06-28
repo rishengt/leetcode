e2619e3c47314fec7f19df111fd0527012b312e1
risheng1996 <rishengt@gmail.com>
2020-06-28 16:07:23 -0400

    subsequence的定义以及数量的找法

--- byName/src/NumberofSubsequencesThatSatisfytheGivenSumCondition.java
+++ byName/src/NumberofSubsequencesThatSatisfytheGivenSumCondition.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Given an array of integers nums and an integer target.
  *
@@ -35,7 +37,23 @@
  * Explanation: All non-empty subset satisfy the condition (2^7 - 1) = 127
  */
 public class NumberofSubsequencesThatSatisfytheGivenSumCondition {
-    public int numSubseq(int[] nums, int target) {
-
+    public static void main(String[] args) {
+        System.out.println(new NumberofSubsequencesThatSatisfytheGivenSumCondition().numSubseq(new int[]{2,3,3,4,6,7}, 12));
+    }
+    public int numSubseq(int[] A, int target) {
+        Arrays.sort(A);
+        int res = 0, n = A.length, l = 0, r = n - 1, mod = (int)1e9 + 7;
+        int[] pows = new int[n];
+        pows[0] = 1;
+        for (int i = 1 ; i < n ; ++i)
+            pows[i] = pows[i - 1] * 2;
+        while (l <= r) {
+            if (A[l] + A[r] > target) {
+                r--;
+            } else {
+                res = (res + pows[r - l++]);
+            }
+        }
+        return res;
     }
 }
