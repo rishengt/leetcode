1f9ff451122675e622c4f5eb3ce19882b6382ae8
risheng1996 <rishengt@gmail.com>
2020-08-03 14:52:55 -0400

    老实讲这道题的推理过程你让过几个小时后的我来做还真不一定做得出来。。。。。

---
+++ byName/src/RotateImage.java
@@ -0,0 +1,56 @@
+/**
+ * You are given an n x n 2D matrix representing an image.
+ *
+ * Rotate the image by 90 degrees (clockwise).
+ *
+ * Note:
+ *
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+ *
+ * Example 1:
+ *
+ * Given input matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ * Example 2:
+ *
+ * Given input matrix =
+ * [
+ *   [ 5, 1, 9,11],
+ *   [ 2, 4, 8,10],
+ *   [13, 3, 6, 7],
+ *   [15,14,12,16]
+ * ],
+ *
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ *   [15,13, 2, 5],
+ *   [14, 3, 4, 1],
+ *   [12, 6, 8, 9],
+ *   [16, 7,10,11]
+ * ]
+ */
+public class RotateImage {
+    /**我艹，再遇到这种题，先盯住一个点，然后找取代他得点，然后总结出与matrix。length的关系，然后推理*/
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for (int i=0; i<n/2; i++) /**因为j会 = i，缩圈，而且 遍历的时候会把后面的也顺便填上了，所以i遍历一半就够了*/
+            for (int j=i; j<n-i-1; j++) { /**j = i 超关键，缩圈了，跑毒了，我顶*/
+                int tmp=matrix[i][j];
+                matrix[i][j]=matrix[n-j-1][i];
+                matrix[n-j-1][i]=matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1]=matrix[j][n-i-1];
+                matrix[j][n-i-1]=tmp;
+            }
+    }
+}
