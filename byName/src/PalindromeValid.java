c03e92b006d34ecb83646147c977e118f7a7763f
risheng1996 <rishengt@gmail.com>
2020-08-03 13:10:16 -0400

    Regex 慢慢积累吧，s = s.replaceAll('[^a-z0-9]','') 非a-z0-9的，\W 搞不动_ underscore

--- byName/src/PalindromeValid.java
+++ byName/src/PalindromeValid.java
@@ -18,6 +18,7 @@ public class PalindromeValid {
         System.out.println(new PalindromeValid().isPalindrome("race a car"));
     }
     public boolean isPalindrome(String s){
+        //s = s.toLowerCase().replaceAll("[^a-z0-9]", ""); 慢是慢但是Regex还是要记一记的
         for(int i = 0, j = s.length()-1; i<j; i++, j--){
             while(i<j&&!Character.isLetterOrDigit(s.charAt(i))/**(!Character.isAlphabetic(s.charAt(i))||!Character.isDigit(s.charAt(i)))这样写不行*/){
                 i++;
