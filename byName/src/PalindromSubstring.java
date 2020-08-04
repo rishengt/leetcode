c8689f4570faff293e1add47e10c4c67bbf7fe5b
risheng1996 <rishengt@gmail.com>
2020-08-04 16:00:31 -0400

    其实一生之敌的expandCenter细品品还是很有滋味的。。。

--- byName/src/PalindromSubstring.java
+++ byName/src/PalindromSubstring.java
@@ -35,4 +35,27 @@ public class PalindromSubstring {
         }
         return ans;
     }
+
+    public String longestPalindromeII(String s){
+        int start = 0, end = 0, maxLen = 0;
+        for(int i = 0; i<s.length(); i++){
+            int len1 = expandFromCenter(s,i,i);/**奇数长度的string，单个中心*/
+            int len2 = expandFromCenter(s,i,i+1);/**偶数长度的string，两个中心*/
+            int tempMaxLen = Math.max(len1,len2);
+            if(tempMaxLen>maxLen){
+                maxLen = tempMaxLen;
+                start = i - (tempMaxLen-1)/2;/**因为目前 i 是中心，所以 要找start你要减去长度的一半*/
+                end = i+tempMaxLen/2;/**这里细节一点都不比dp少啊，一个不注意就挂了*/
+            }
+        }
+        return s.substring(start,end+1);
+    }
+
+    public int expandFromCenter(String s, int i, int j){
+        while(i>=0&&j<s.length()&&s.charAt(i)==s.charAt(j)){
+            i--;
+            j++;
+        }
+        return j-i-1;/**这里的细节也很重要。。。*/
+    }
 }
