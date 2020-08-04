cc7cb1d4f993c3c990d77fca14190b65dfef8681
risheng1996 <rishengt@gmail.com>
2020-08-04 12:45:48 -0400

    我是真他妈佩服大神的脑回路哈哈哈哈

---
+++ byName/src/RotateArrayRight.java
@@ -0,0 +1,61 @@
+/**
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ *
+ * Follow up:
+ *
+ * Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,4,5,6,7], k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * Example 2:
+ *
+ * Input: nums = [-1,-100,3,99], k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 2 * 10^4
+ * It's guaranteed that nums[i] fits in a 32 bit-signed integer.
+ * k >= 0
+ */
+public class RotateArrayRight {
+
+    /***********************************这神一样的脑回路比我以前瞎写的那个快版的好记多了，佩服哈哈哈哈****************************/
+    /**
+     * nums = "----->-->"; k =3
+     * result = "-->----->";
+     *
+     * reverse "----->-->" we can get "<--<-----"
+     * reverse "<--" we can get "--><-----"
+     * reverse "<-----" we can get "-->----->"
+     * this visualization help me figure it out :)
+     */
+    public void rotate(int[] nums, int k) {
+        k%=nums.length;
+        reverse(nums,0, nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
+    public void reverse(int[] nums, int i, int k){
+        while(i<k){
+            swap(nums,i++,k--);
+        }
+    }
+    public void swap(int[] nums, int i, int k){
+        int temp = nums[i];
+        nums[i] = nums[k];
+        nums[k] = temp;
+    }
+}
