b3d3ff4b2f4b66c9b127ab7acb19353cfefbd9af
risheng1996 <rishengt@gmail.com>
2020-08-04 22:11:55 -0400

    审题还是要再仔细一点，而且Gary兄说的很有道理，要向最优解看齐。。妈的菜🐶

--- byName/src/LargestBSTSubtree.java
+++ byName/src/LargestBSTSubtree.java
@@ -1,7 +1,10 @@
 import com.sun.tools.corba.se.idl.toJavaPortable.Helper;
 
+import java.util.*;
+
 /**
- * Given a binary tree, find the largest subtree which is a Binary Search Tree (BST), where largest means subtree with largest number of nodes in it.
+ * Given a binary tree, find the largest subtree which is a Binary Search Tree (BST),
+ * where largest means subtree with largest number of nodes in it.
  *
  * A subtree must include all of its descendants.
  * 样例
@@ -33,6 +36,17 @@ import com.sun.tools.corba.se.idl.toJavaPortable.Helper;
  * Can you figure out ways to solve it with O(n) time complexity?
  */
 public class LargestBSTSubtree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+        root.right.right.left = new TreeNode(1);
+//        TreeNode root = new TreeNode(3);
+//        root.left = new TreeNode(1);
+//        System.out.println(new LargestBSTSubtree().largestBSTSubtree(root));//2  why???
+        System.out.println(new LargestBSTSubtree().largestBSTSubtreeII(root));//1
+    }
     int max = 0;
     public int largestBSTSubtree(TreeNode root) {
         helper(root);
@@ -56,12 +70,52 @@ public class LargestBSTSubtree {
         return res;
     }
 
-    public boolean isBST(TreeNode root){
-
+    public int largestBSTSubtree(TreeNode root){
+        if(root == null) return 0;
+        int max = Integer.MIN_VALUE;
+        List<TreeNode> list = new ArrayList<>();
+        FindBST(root,list);
+        Queue<int[]> queue = new PriorityQueue<>((a,b)->b[0]-a[0]);
+        for(TreeNode node: list){
+            queue.offer(getSizeAndMax(node,max));
+        }
+        return queue.poll()[0];
     }
-    public boolean helper(TreeNode root, int max, int min){
+
+    public int[] getSizeAndMax(TreeNode root, int max){
+        int ans = 0;
+        if(root == null) return new int[]{ans,0};
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(root);
+        while(!deque.isEmpty()){
+            int size = deque.size();
+            ans += size;
+            for(int i = 0; i<size; i++){
+                TreeNode temp = deque.poll();
+                if(temp.val>max) max = temp.val;
+                if(temp.left!=null)deque.offer(temp.left);
+                if(temp.right!=null)deque.offer(temp.right);
+            }
+        }
+        return new int[]{ans,max};
+     }
+
+    public void FindBST(TreeNode root, List<TreeNode> list){
+        if(root == null) return;
+        if(isBST(root)){
+            list.add(root);
+        }
+        FindBST(root.left,list);
+        FindBST(root.right,list);
+    }
+
+    public boolean isBST(TreeNode root){
+        return isBST(root,null,null);
+    }
+    public boolean isBST(TreeNode root, TreeNode max, TreeNode min){
         if(root == null) return true;
-        if(root.val>max || root.val<min) return false;
-        return helper(root.left,root.val,Integer.MIN_VALUE) && helper(root.right,Integer.MAX_VALUE,root.val) ;
+        if( max!=null && root.val>=max.val) return false;
+        if( min!=null && root.val<=min.val) return false;
+        return isBST(root.left,root,min) && isBST(root.right,max,root) ;
     }
 }
