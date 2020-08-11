abd2966e4d56e41af187212e1354f2cddc06ea1b
risheng1996 <rishengt@gmail.com>
2020-08-11 00:31:35 -0400

    Integer 的valueOf只能搞String跟int呢。。。

---
+++ BasicKnowledge/src/Integer/ConvertStringNumtoInteger.java
@@ -0,0 +1,9 @@
+package Integer;
+
+public class ConvertStringNumtoInteger {
+    public static void main(String[] args) {
+        String s = "2";
+        int k = Integer.valueOf(2);
+        System.out.println(k+1);
+    }
+}
