f0adb345fcb351fa5b95bf930db1d0fe81ee6876
risheng1996 <rishengt@gmail.com>
2020-08-11 11:27:05 -0400

    👴真的吐了，switch后面加个小括号里面放要被换的东西，然后大括号里面写替换逻辑，case 跟替换的实质东西加：，最后别忘了break或者continue。。。

---
+++ BasicKnowledge/src/Switch/SwitchStatement.java
@@ -0,0 +1,14 @@
+package Switch;
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+        String s = "1";
+        switch (s){
+            case "1":
+                System.out.println("cnm");
+                break;
+            case "2":
+                System.out.println("cnb");
+        }
+    }
+}
