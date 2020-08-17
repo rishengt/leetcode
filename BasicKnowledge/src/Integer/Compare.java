a23d828cb43b966b5a8665eceb64708b899a76bc
risheng1996 <rishengt@gmail.com>
2020-08-17 13:42:37 -0400

    Integer and Double class都有compare(a,b)这方法，要是a比b大就输出1，等于输出0，小于输出-1

---
+++ BasicKnowledge/src/Integer/Compare.java
@@ -0,0 +1,10 @@
+package Integer;
+
+public class Compare {
+    public static void main(String[] args) {
+        System.out.println(Integer.compare(1,3));/**-1*/
+        System.out.println(Integer.compare(3,1));
+        System.out.println(Integer.compare(2,2));
+        System.out.println(Double.compare(1.0,Double.MAX_VALUE));/**-1*/
+    }
+}
