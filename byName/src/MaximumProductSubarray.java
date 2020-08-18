53bb6c842e946e04ad1927bba359b0a43427d6d4
risheng1996 <rishengt@gmail.com>
2020-08-17 20:02:12 -0400

    这题跟MaximumSubarray非常像，但要多一个心眼保留一个变量存最小值因为负负得正。。。

---
+++ byName/src/MaximumProductSubarray.java
@@ -0,0 +1,32 @@
+/**
+ * Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+ *
+ * Example 1:
+ *
+ * Input: [2,3,-2,4]
+ * Output: 6
+ * Explanation: [2,3] has the largest product 6.
+ * Example 2:
+ *
+ * Input: [-2,0,-1]
+ * Output: 0
+ * Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ */
+public class MaximumProductSubarray {
+    public static void main(String[] args) {
+        System.out.println(new MaximumProductSubarray().maxProduct(new int[]{2,3,-2,4}));
+        System.out.println(new MaximumProductSubarray().maxProduct(new int[]{-2,0,-1}));
+        System.out.println(new MaximumProductSubarray().maxProduct(new int[]{-2,3,-4}));
+    }
+    public int maxProduct(int[] nums) {
+        int cur = nums[0];
+        int max = nums[0];
+        int min = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            cur = Math.max(nums[i], Math.max(cur*nums[i], min*nums[i]));
+            min = Math.min(nums[i], Math.min(cur*nums[i],min*nums[i]));
+            max = Math.max(max,cur);
+        }
+        return max;
+    }
+}
