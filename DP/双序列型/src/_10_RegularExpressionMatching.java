0979718be10cd52c1d78f60b82db41bb252ab11b
risheng1996 <rishengt@gmail.com>
2020-08-28 15:43:15 -0400

    随机数还能这样玩，蛮子面试原题。妙啊，你回头在byname上自己再做一遍

---
+++ DP/双序列型/src/_10_RegularExpressionMatching.java
@@ -0,0 +1,56 @@
+/**
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+ *
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ *
+ * Note:
+ *
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ * Example 1:
+ *
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * Example 2:
+ *
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+ * Example 3:
+ *
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * Example 4:
+ *
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+ * Example 5:
+ *
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ */
+public class _10_RegularExpressionMatching {
+    /**双序列套路：
+     *  dp[i][j]: 表示s[1:i] 和 p[1:j]的子问题的解
+     *  想方设法把 dp[i][j] 向 dp[i-1][j], dp[i][j-1] 和 dp[i-1][j-1] 转移
+     *  最终答案 dp[s.length()][p.length()]
+     */
+//    public boolean isMatch(String s, String p){
+//
+//    }
+}
