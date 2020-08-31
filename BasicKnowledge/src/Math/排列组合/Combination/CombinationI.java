a1faf7a8b02dfd3b6ba6a41093cb4486d746a8fd
risheng1996 <rishengt@gmail.com>
2020-08-31 14:45:45 -0400

    组合数与pascal triangle的谜之关系。。妙啊！！而且那两题杨辉三角的解法真的看湿老子！！

--- BasicKnowledge/src/Math/排列组合/Combination/CombinationI.java
+++ BasicKnowledge/src/Math/排列组合/Combination/CombinationI.java
@@ -4,11 +4,23 @@ package Math.排列组合.Combination;
  *  求从a个数里面选b个数进行组合的方案数， note： a个不同的数
  */
 public class CombinationI {
-    public int com(int a, int b){
-        if(a == 0 || a == b){
-            return 1;
-        }else{     /**选一个a里面选一个符合b的情况加a里面选一个但不符合b的情况 = 总情况*/
-            return com(a-1,b-1)+com(a-1,b);
+    public static void main(String[] args) {
+        long[][] c = new CombinationI().getTriangle(4);
+        System.out.println(c[4][4]);
+    }
+
+    private long[][] getTriangle(int n) {
+        // Yang Hui (Pascle) triangle
+        // 4C2 = triangle[4][2] = 6
+        long[][] triangle = new long[n+1][n+1];
+        for (int i = 0; i <= n; i++) {
+            triangle[i][0] = triangle[i][i] = 1;
         }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] = (triangle[i - 1][j] + triangle[i - 1][j - 1]);
+            }
+        }
+        return triangle;
     }
 }
