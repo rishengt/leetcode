0baf103709a23d558017175ff68c2b459a9bd49b
risheng1996 <rishengt@gmail.com>
2020-08-31 09:57:12 -0400

    if else 的精髓跟dp 递推，这道题赚到了啊。。。

--- byName/src/MaximumLengthofSubarrayWithPositiveProduct.java
+++ byName/src/MaximumLengthofSubarrayWithPositiveProduct.java
@@ -42,7 +42,35 @@ public class MaximumLengthofSubarrayWithPositiveProduct {
     public static void main(String[] args) {
         System.out.println(new MaximumLengthofSubarrayWithPositiveProduct().getMaxLen(new int[]{-1,-2}));
     }
-    public int getMaxLen(int[] nums) {
+    /**下面那个应该是最优解，空间复杂度最小，但是真的很难领悟跟记住，还是递推dp好记，想想看这是属于什么dp*/
+    /**不属于Guan神总结的那些dp套路但应该可以看成是线性dp吧***/
+    /**我们定义一个f(i) 表示第i位的时候最长的正subarray， g(i)表示第i位时最长的负subarray
+     * 1。nums[i] > 0 f(i) = f(i-1)+1 and g(i) = g(i-1) + 1;
+     * 2. nums[i] = 0 f(i) = 0; g(i) = 0;
+     * 3. nums[i] <0 f(i) = g(i-1)+1; g(i) = f(i-1)+1;
+     */
+    public int getMaxLen(int[] nums){
+        int dp1[] = new int[nums.length];
+        int dp2[] = new int[nums.length];
+        if(nums[0] > 0) dp1[0] = 1;
+        else if(nums[0]<0) dp2[0] = 1;
+        int max = Integer.MIN_VALUE;
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i]>0){
+                dp1[i] = dp1[i-1]+1;
+                if(dp2[i-1]>0) dp2[i] = dp2[i-1]+1;/**这个应该是特殊情况，你前面压根没有负的那你永远变不成负的，所以要先判断你前面有没有负数*/
+            }
+            else if(nums[i]<0){
+                dp2[i] = dp1[i-1] + 1;
+                if(dp2[i-1]>0) dp1[i] = dp2[i-1]+1;/**同理。。。*/
+            }
+            max = Math.max(dp1[i],max);
+        }
+        return max;
+    }
+
+
+    public int getMaxLenII(int[] nums) {
         // sum is used to count the number of negative numbers from zeroPosition to current index
         int firstNegative = -1, zeroPosition = -1, sum = 0, max = 0;
         for(int i = 0;i < nums.length; i++){
