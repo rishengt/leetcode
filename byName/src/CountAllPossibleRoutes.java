2acb8354d9f7a04e83f100d33c544cfb5b0fd1fc
risheng1996 <rishengt@gmail.com>
2020-09-06 17:08:05 -0400

    这道题用memoization实现起来超级优雅，希望你自己可以做出来。。菜🐔！

--- byName/src/CountAllPossibleRoutes.java
+++ byName/src/CountAllPossibleRoutes.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+
 /**
- * You are given an array of distinct positive integers locations where locations[i] represents the position of city i. You are also given integers start, finish and fuel representing the starting city, ending city, and the initial amount of fuel you have, respectively.
+ * You are given an array of distinct positive integers locations where locations[i] represents the position of city i.
+ * You are also given integers start, finish and fuel representing the starting city, ending city, and the initial amount of fuel you have, respectively.
  *
- * At each step, if you are at city i, you can pick any city j such that j != i and 0 <= j < locations.length and move to city j. Moving from city i to city j reduces the amount of fuel you have by |locations[i] - locations[j]|. Please notice that |x| denotes the absolute value of x.
+ * At each step, if you are at city i, you can pick any city j such that j != i and 0 <= j < locations.length and move to city j.
+ * Moving from city i to city j reduces the amount of fuel you have by |locations[i] - locations[j]|.
+ * Please notice that |x| denotes the absolute value of x.
  *
  * Notice that fuel cannot become negative at any point in time, and that you are allowed to visit any city more than once (including start and finish).
  *
@@ -56,7 +61,32 @@
  * 1 <= fuel <= 200
  */
 public class CountAllPossibleRoutes {
-//    public int countRoutes(int[] locations, int start, int finish, int fuel) {
-//
-//    }
+    public static void main(String[] args) {
+        System.out.println(new CountAllPossibleRoutes().countRoutes(new int[]{1,2,3,},0,2,40));
+    }
+    /**dp[i][rest] 定义你目前的城市以及剩下的油量，遍历所有可以去的城市，要是到达目的地就奖励自己一个路径。保存起来, 然后继续跑，跑到油用光为止**/
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        int dp[][] = new int[locations.length][fuel+1];
+        for(int i = 0; i<locations.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return dfs(dp,locations,start,finish,fuel);
+    }
+
+    public int dfs(int[][] dp , int[] locations, int start, int finish, int fuel){
+        if(dp[start][fuel] != -1) return dp[start][fuel];
+        int count = 0;
+        for(int i = 0; i<locations.length; i++){
+            if(i == start) continue;
+            if(i == finish && fuel - Math.abs(locations[i]-locations[start])>= 0){
+                count++;
+                count %= 1000000007;
+            }
+            if(fuel - Math.abs(locations[i]-locations[start])>= 0)
+                count += dfs(dp,locations,i, finish,fuel - Math.abs(locations[i]-locations[start]));
+                count %= 1000000007;
+        }
+        dp[start][fuel] = count;
+        return count;
+    }
 }
