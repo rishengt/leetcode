921e057e91dadfee0d1cb966c3a8d52e4232e7fd
risheng1996 <rishengt@gmail.com>
2020-09-08 13:12:46 -0400

    基本的常识又增加了，但还是挺难记的，但是码农你连标准库的区区几个函数都记不住还想让你去记dp的模版？？？吃屎啦你

--- BasicKnowledge/src/Integer/ConvertStringNumtoInteger.java
+++ BasicKnowledge/src/Integer/ConvertStringNumtoInteger.java
@@ -5,5 +5,7 @@ public class ConvertStringNumtoInteger {
         String s = "2";
         int k = Integer.valueOf(2);
         System.out.println(k+1);
+        String bit = "01000101";
+        System.out.println(Integer.parseInt(bit,2));/**这也不是第一次遇到了，后面的数字告诉了前面的string是什么样的进制，parse过来输出都是10进制的*/
     }
 }
--- BasicKnowledge/src/StringBuilder/delete.java
+++ BasicKnowledge/src/StringBuilder/delete.java
@@ -4,5 +4,6 @@ public class delete {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         sb.delete(1,2);
+        sb.deleteCharAt(2);
     }
 }
